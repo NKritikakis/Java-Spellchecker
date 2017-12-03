21b3b0c39cc5b98edf99dcd5ae85cda7032835c0
Antonis Faros <antonis.faros@gmail.com>
2017-12-03 17:23:31 +0200

    Add files via upload
    Class for document reading through keyboard(.txt files)

---
+++ DocumentReader.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class DocumentReader extends Thread {
+	@Override
+	//Needs an object to run e.x DocumentReader object1 = new DocumentReader();
+	//object1.start();
+	public void run() {
+		System.out.print("Please insert the file's name:");
+		Scanner keyboard = new Scanner (System.in);
+		String fileName = keyboard.next();
+		Scanner inputStream = null;
+		try {
+			inputStream = new Scanner(new File(fileName));
+		} catch(FileNotFoundException e) {
+			System.out.println("Error opening the file" +fileName);
+			System.exit(0);
+		}
+		while(inputStream.hasNextLine()) {
+			// to our HashMap
+		}
+
+
+	}
+}
