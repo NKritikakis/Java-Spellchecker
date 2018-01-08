27cedf10f1e0a8b7242211aa1656bf3093330af0
giannismarinis <32640346+giannismarinis@users.noreply.github.com>
2018-01-08 16:51:22 +0200

    Add javadoc and style changes

--- SplitText.java
+++ SplitText.java
@@ -1,11 +1,21 @@
 import java.util.Scanner;
+
+/**
+* Some comments for that class ... 
+* 
+* @author  Author name
+* @version 1.0
+*/
 public class SplitText {
-	Scanner input = new Scanner (System.in);
-	String text = input.next();
-	String inputText[] = text.split(" ");
-	public String[] getTextArray() {
-		return inputText;
-	}
+  Scanner input = new Scanner(System.in);
+  String text = input.next();
+  String inputText[] = text.split(" ");
+
+  /**
+  * This method is used ...
+  * @return string This returns ... .
+  */
+  public String[] getTextArray() {
+    return inputText;
+  }
 }
-
-
