6c5e573c995e1e6b0d1219c265f8e0cd07bed269
giannismarinis <32640346+giannismarinis@users.noreply.github.com>
2018-01-08 16:54:11 +0200

    Add Javadoc and style changes.

--- DocumentReader.java
+++ DocumentReader.java
@@ -2,26 +2,35 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+/**
+* Some comments for that class ... 
+* 
+* @author  Author name
+* @version 1.0
+*/
 public class DocumentReader extends Thread {
-	@Override
-	//Needs an object to run e.x DocumentReader object1 = new DocumentReader();
-	//object1.start();
-	public void run() {
-		System.out.print("Please insert the file's name:");
-		Scanner keyboard = new Scanner (System.in);
-		String fileName = keyboard.next();
-		Scanner inputStream = null;
-		try {
-			inputStream = new Scanner(new File(fileName));
-		} catch(FileNotFoundException e) {
-			System.out.println("Error opening the file" +fileName);
-			System.exit(0);
-		}
-		while(inputStream.hasNextLine()) {
-			// to our HashMap myMap = class.gethashMap();
-			//myMap.add(inputStream.nextLine());
-		}
+  @Override
+  // Needs an object to run e.x DocumentReader object1 = new DocumentReader();
+  // object1.start();
+  /**
+  * Some comments ..
+  * ....
+  */
+  public void run() {
+    System.out.print("Please insert the file's name:");
+    Scanner keyboard = new Scanner(System.in);
+    String fileName = keyboard.next();
+    Scanner inputStream = null;
+    try {
+      inputStream = new Scanner(new File(fileName));
+    } catch (FileNotFoundException e) {
+      System.out.println("Error opening the file" + fileName);
+      System.exit(0);
+    }
+    while (inputStream.hasNextLine()) {
+      // to our HashMap myMap = class.gethashMap();
+      // myMap.add(inputStream.nextLine());
+    }
 
-
-	}
+  }
 }
