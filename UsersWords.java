91794aaba4b3be0da41bfae1f92d66ae01d33d40
giannismarinis <32640346+giannismarinis@users.noreply.github.com>
2018-01-08 16:52:28 +0200

    Add Javadoc and style changes.

--- UsersWords.java
+++ UsersWords.java
@@ -1,20 +1,43 @@
 import java.util.LinkedList;
+
+/**
+* Some comments for that class ... 
+* 
+* @author  Author name
+* @version 1.0
+*/
 public class UsersWords {
-	static LinkedList<String> myWords = new LinkedList<String>();
-	public UsersWords(String input) {
-		myWords.add(input);
-	}
-	public static LinkedList<String> getUsersWords() {
-		return myWords;
-	}
-	public static boolean CheckforWord(String wordToSearch) {
-		if (myWords.contains(wordToSearch)) {
-			return true;
-		}else {
-			return false;
-		}
-	}
+  // describe that variable here ...
+  static LinkedList<String> myWords = new LinkedList<String>();
 
+  /**
+   * This method is used ...
+   * @param input String: what we do with that variable
+   * @return LinkedList<String> This returns ... .
+   */
+  public UsersWords(String input) {
+    myWords.add(input);
+  }
 
+  /**
+   * This method is used ...
+   * @return LinkedList<String> This returns ... .
+   */
+  public static LinkedList<String> getUsersWords() {
+    return myWords;
+  }
+
+  /**
+   * This method is used ...
+   * @param wordToSearch String: what we do with that variable
+   * @return true This returns ... .
+   */
+  public static boolean CheckforWord(String wordToSearch) {
+    if (myWords.contains(wordToSearch)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 }
